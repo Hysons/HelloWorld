9d21127577545d118c70832ccdc6803280df98ea
Hysons <707061649@qq.com>
2024-08-09 09:53:32 +0800

    linear regression

--- practice4/pt01_linear regression.py
+++ practice4/pt01_linear regression.py
@@ -50,7 +50,9 @@ class LinearRegression(nn.Module):
 model = LinearRegression(input_size, output_size)
 
 # Loss and Optimizer
+#调用均方差函数MSE做损失函数。第一次运行报错参数模糊不清源于该处方法的括号缺失。
 ctiterion = nn.MSELoss()
+#输入需要训练的模型参数，并传入学习速率超参数来初始化优化器。
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 # Train the Model
@@ -60,21 +62,34 @@ for epoch in range(num_epochs):
     targets = Variable(torch.from_numpy(y_train))
 
     # Forward + Backward + Optimize
+    #遍历模型的所有参数。通过p.grad.detach_()方法截断反向传播的梯度流，再通过p.grad.zero_()函数将每个参数的梯度值设为0，即上一次的梯度记录被清空。
     optimizer.zero_grad()
+    #返回模型前向传播中输出层的值到outputs
     outputs = model(inputs)
+    #传入计算值和实际值，调用均方差做损失函数
     loss = ctiterion(outputs, targets)
+    #loss会一层层的反向传播计算每个w的梯度值，并保存到该w的.grad属性中。
     loss.backward()
+    #执行一次优化步骤，通过梯度下降法来更新参数的值。因为梯度下降是基于梯度的，所以在执行optimizer.step()函数前应先执行loss.backward()函数来计算梯度。
     optimizer.step()
 
+    #每隔5次输出一次损失率
     if (epoch + 1) % 5 == 0:
         print('Epoch [%d/%d], Loss: %.4f'.format(epoch + 1, num_epochs, loss.item()))
 
 # Plot the graph
 
-
+#将模型设置为评估模式，需要关闭一些在训练过程中使用的特性，如Dropout和BatchNorm层的训练模式。在评估模式下，模型将使用训练过程中学到的参数进行前向传播，而不会更新这些参数。
 model.eval()
+#关闭梯度计算，可以节省内存和计算资源
+torch.no_grad()
+#输入x轴训练集，输出模型结果作为预测值
 predicted = model(Variable(torch.from_numpy(x_train))).data.numpy()
+#绘制原始数据，‘ro-’ 表示红色圆点线条。
 plt.plot(x_train, y_train, 'ro')
+#绘制线性回归曲线
 plt.plot(x_train, predicted, label='predict')
+#添加图例。如显示标签'predict'
 plt.legend()
+#图显示出来
 plt.show()
